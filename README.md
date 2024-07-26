9d051441751f5f1645f21c65de16662e98fb8d16
Yan Holtz <yan.holtz.data@gmail.com>
2024-07-26 10:53:21 +0200

    good typography

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Features:
 - Deploy on gh-pages on push with a github action
   - (Note: on github.com, go to pages and select deploy from branch gh-pages)
   - note: go to settings -> action -> general and change "workflow permission" to allow read AND write permisisons
+- Good looking typography based on shadCnUI
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,56 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
-    <main className="container mx-auto px-4 py-6 " style={{ maxWidth: 900 }}>
+    <main className="container mx-auto px-4 py-6 " style={{ maxWidth: 600 }}>
       <div>
-        <p>Hello</p>
+        <h1>The Joke Tax Chronicles</h1>
+
+        <p>
+          Once upon a time, in a far-off land, there was a very lazy king who
+          spent all day lounging on his throne. One day, his advisors came to
+          him with a problem: the kingdom was running out of money.
+        </p>
+
+        <h2>The King's Plan</h2>
+        <p>
+          The king thought long and hard, and finally came up with{" "}
+          <a
+            href="#"
+            className="font-medium text-primary underline underline-offset-4"
+          >
+            a brilliant plan
+          </a>
+          : he would tax the jokes in the kingdom.
+        </p>
+        <blockquote>
+          "After all," he said, "everyone enjoys a good joke, so it's only fair
+          that they should pay for the privilege."
+        </blockquote>
+        <h3>The Joke Tax</h3>
+        <p>
+          The king's subjects were not amused. They grumbled and complained, but
+          the king was firm:
+        </p>
+        <ul>
+          <li>1st level of puns: 5 gold coins</li>
+          <li>2nd level of jokes: 10 gold coins</li>
+          <li>3rd level of one-liners : 20 gold coins</li>
+        </ul>
+        <p>
+          As a result, people stopped telling jokes, and the kingdom fell into a
+          gloom. But there was one person who refused to let the king's
+          foolishness get him down: a court jester named Jokester.
+        </p>
+
+        <h3>The People's Rebellion</h3>
+        <p>
+          The people of the kingdom, feeling uplifted by the laughter, started
+          to tell jokes and puns again, and soon the entire kingdom was in on
+          the joke.
+        </p>
+
         <Button>This is a button</Button>
       </div>
     </main>
